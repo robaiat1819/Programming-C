cb7e8206b3a6d9fc003b70016aaa8614c1f4beae
robaiat181 <fardinrobaiat@gmail.com>
2025-03-08 00:38:15 +0600

    Push Module 6

---
+++ Module-6/Neste_For-Loop.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    for(int j=1;j<=5;j++)
+    {
+        for(int j=1;j<=5;j++)
+    {
+        printf("%d ",j);
+    } 
+    printf("\n");
+    }
+    return 0;
+}
+
+
+
+
+
+#include<stdio.h>
+int main()
+{
+    int i = 10;
+while (i >= 0) {
+    printf("%d ", i);
+    i -= 2;
+}
+
+    return 0;
+}
